c693f8cdb534633f27cfb259b50d8dddbd4cee95
[tami888] <[tait8pcvitxter8tami@gmail.com]>
2016-12-16 17:42:25 +0900

    変数の誤代入を修正

--- Problem002.py
+++ Problem002.py
@@ -11,9 +11,8 @@ num1 = 1
 num2 = 1
 ans = 0
 
-while num2 <= 4000000:
+while num2 < 4000000:
     if num2 % 2 == 0:
         ans += num2
-    num1 = num2
-    num2 = num1 + num2
+    num1, num2 = num2, num1 + num2
 print ans
