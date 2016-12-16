204821c7a893b7fb888adce654fcadd7c2308612
[tami888] <[tait8pcvitxter8tami@gmail.com]>
2016-12-16 11:09:37 +0900

    問題文追加、変数tmpを削除

--- Problem002.py
+++ Problem002.py
@@ -1,13 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+フィボナッチ数列の項は前の2つの項の和である. 最初の2項を 1, 2 とすれば, 最初の10項は以下の通りである.
+
+1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
+数列の項の値が400万以下の, 偶数値の項の総和を求めよ.
+"""
+
+
 num1 = 1
-num2 = 2
-tmp = 0
+num2 = 1
+ans = 0
 
-sum1 = 2
-while tmp <= 4000000:
-    tmp = num1 + num2
-    if tmp % 2 == 0:
-        sum1 = sum1 + tmp
-
+while num2 <= 4000000:
+    if num2 % 2 == 0:
+        ans += num2
     num1 = num2
-    num2 = tmp
-print sum1
+    num2 = num1 + num2
+print ans
